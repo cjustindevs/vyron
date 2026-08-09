fc73942c373e8b86426d62ff6f9c1a28444e39e7
CJ Lumanglas <justincarltz14@gmail.com>
2026-08-10 00:25:58 +0800

    Force HTTPS URL generation in production (fixes mixed-content blocked assets)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Render serves the app over HTTPS; force secure URLs for assets,
+        // forms and route generation even if APP_URL is set to http://...
+        if ($this->app->isProduction()) {
+            URL::forceScheme('https');
+
+            if (str_starts_with(config('app.url'), 'http://')) {
+                URL::forceRootUrl(str_replace('http://', 'https://', config('app.url')));
+            }
+        }
     }
 }
